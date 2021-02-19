7f3b6feec6788bddc55aeb8e0f92de984983a5ee
belenbernal <beelu.bernal@gmail.com>
2021-02-19 01:11:49 -0300

    creo vista de edit, rutas y metodos de carga

--- food4me/controllers/productController.js
+++ food4me/controllers/productController.js
@@ -1,8 +1,36 @@
+const fs = require("fs");
 const products = require('../data/products')
 
 const productController = {
     productAdd: (req, res)=>{ //method get
         res.render('productAdd')
+    },
+    newProduct: (req, res,next)=>{ //method post
+        /*res.send(req.body)*/
+        let lastID = 0;
+        products.forEach(product => {
+            if (product.id > lastID) {
+                lastID = product.id
+            }
+        });
+
+        const {name, description, price, client, image, category, vegan, vegetarian, celiac} = req.body;
+
+        let product = {
+            id: lastID + 1,
+            name, 
+            description, 
+            price, 
+            client, 
+            image : req.files[0].filename, 
+            category, 
+            vegan, 
+            vegetarian, 
+            celiac
+        }
+        products.push(product);
+        fs.writeFileSync('./data/products.json' , JSON.stringify(products), 'utf-8');
+        res.redirect('/');
     },
     productDetail: (req, res)=>{
         let product = products.find(product=>{
@@ -12,14 +40,24 @@ const productController = {
             product
         })
     },
+    editProduct: (req, res)=>{ //method get
+        let product = products.find(product=> product.id === +req.params.id
+        );
+        res.render('productEdit', {
+            product
+        })
+    },
+    updateProduct: (req, res,next)=>{ //method post
+        
+    },
     carrito: (req, res)=>{
         res.render('carrito')
     },
-    search: (req, res)=>{
+    search: (req, res)=>{ //falta validacion y editar vista
         const busqueda = req.query.buscar;
 
         const result = products.filter(product =>{
-            return product.name.includes(busqueda);
+            return product.name.toLowerCase().includes(busqueda.toLowerCase());
         });
         res.render('index',{
             products : result
--- food4me/routes/products.js
+++ food4me/routes/products.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path")
+const multer = require("multer");
 const productController = require('../controllers/productController')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/products')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+
+var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/create', productController.productAdd);
+router.post('/create', upload.any(), productController.newProduct);
 
 router.get('/detail/:id', productController.productDetail);
+
+router.get('/edit/:id',productController.editProduct);
+router.post('/update/:id', upload.any(), productController.updateProduct);
 
 router.get('/search', productController.search);
 
