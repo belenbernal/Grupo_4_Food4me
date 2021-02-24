4c17468969961e30419dc469fa73fc8c7145ae48
Franco <franconievas2002@gmail.com>
2021-02-24 10:05:19 -0300

    users

--- food4me/routes/users.js
+++ food4me/routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 const usersController=require('../controllers/usersController')
+
+
 /* GET users listing. */
 router.get('/login', usersController.login)
 router.get('/register', usersController.register)
