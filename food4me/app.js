dc4b3b1c0d90041c218acdfbad3d0111654829b8
belenbernal <beelu.bernal@gmail.com>
2021-02-27 18:30:23 -0300

    agrego controller, validaciones y middlewares de login

--- food4me/app.js
+++ food4me/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require('method-override');
+const session = require('express-session')
+
+var localsCheck = require('./middlewares/localsCheck');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret : "Secreto!!"
+}));
+
+app.use(localsCheck);
 
 app.use('/', indexRouter);
 app.use('/products',productRouter)
