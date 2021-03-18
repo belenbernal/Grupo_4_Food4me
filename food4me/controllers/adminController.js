dd35a3a9776c457f9a5fe66ea7108d49f81b295a
belenbernal <beelu.bernal@gmail.com>
2021-03-18 20:19:23 -0300

    modifico controllers y views

--- food4me/controllers/adminController.js
+++ food4me/controllers/adminController.js
@@ -1,56 +1,48 @@
-const fs = require('fs');
 const path = require('path');
-const { setProduct, getProduct } = require("../data/products");
-const products = getProduct()
 const db = require('../database/models')
 
 const adminController = {
-   
+
     productList: (req, res) => {
-        db.Productos.findAll()
-            .then((products)=>{
-                res.render('admin/indexAdmin', {products})
+        db.Productos.findAll({
+            order: [
+                ['name', 'ASC']
+            ]
+        })
+            .then((products) => {
+                res.render('admin/indexAdmin', { products })
             })
-            .catch((error)=> res.send(error))
+            .catch((error) => res.send(error))
     },
     productAdd: (req, res) => {
         res.render('admin/productAdd')
     },
     newProduct: (req, res, next) => {
-        let lastID = 0;
-        products.forEach(product => {
-            if (product.id > lastID) {
-                lastID = product.id
-            }
-        });
-
         const { name, price, client, image, category, vegan, vegetarian, celiac } = req.body;
-
-        let product = {
-            id: lastID + 1,
-            name,            
+        
+        db.Productos.create({
+            name,
+            description,
             price,
-            client,
-            image: req.files[0].filename,
-            category,
-            vegan,
-            vegetarian,
-            celiac
-        }
-        products.push(product);
-        setProduct(products);
-        res.redirect('/admin/list');
-    },
-    editProduct: (req, res) => { //method get
-        let product = products.find(product => product.id === +req.params.id
-        );
-        res.render('admin/productEdit', {
-            product
+            image:req.files[0].filename,
+            category_id: category,
+            client_id :client
         })
+        .then(()=>{
+            res.redirect('/admin/list')
+        })
+        .catch((error)=>{res.send(error)})        
     },
-    updateProduct: (req, res, next) => { //method put
+    editProduct: (req, res) => {
+        db.Productos.findByPk(req.params.id)
+            .then((product) => {
+                res.render("admin/productEdit", {product})
+            })
+            .catch((error) => {res.send(error)})
+    },
+    updateProduct: (req, res, next) => {
 
-        const { name, price, client, image, category, vegan, vegetarian, celiac } = req.body;
+        /* const { name, price, client, image, category, vegan, vegetarian, celiac } = req.body;
 
         products.forEach(product => {
             if (product.id === +req.params.id) {
@@ -58,7 +50,7 @@ const adminController = {
                     fs.unlinkSync(path.join('public', 'images', 'products', product.image));
                 }
                 product.id = +req.params.id;
-                product.name = name;                
+                product.name = name;
                 product.price = price;
                 product.client = client;
                 product.image = req.files[0].filename;
@@ -70,22 +62,19 @@ const adminController = {
             }
         });
         setProduct(products)
-        res.redirect('/admin/list');
+        res.redirect('/admin/list'); */
     },
     productDelete: (req, res) => {
-        products.forEach(product => {
-            if (product.id === +req.params.id) {
-                if (fs.existsSync(path.join('public', 'images', 'products', product.image))) {
-                    fs.unlinkSync(path.join('public', 'images', 'products', product.image));
-                }
-                let eliminar = products.indexOf(product);
-                products.splice(eliminar, 1);
+        db.Productos.destroy({
+            where: {
+                id: req.params.id
             }
-        });
-        setProduct(products)
-        res.redirect('/admin/list');
-
-    }
+        })
+            .then(() => {
+                return res.redirect('/admin/list');
+            })
+            .catch(error => res.send(error))
+    } 
 }
 
 module.exports = adminController
