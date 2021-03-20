893bc94b474e229317af0aac901333072d52bbd6
belenbernal <beelu.bernal@gmail.com>
2021-03-20 20:09:37 -0300

    edicion de producto

--- food4me/controllers/adminController.js
+++ food4me/controllers/adminController.js
@@ -18,74 +18,89 @@ const adminController = {
         res.render('admin/productAdd')
     },
     newProduct: (req, res, next) => {
-        const { name, price, category, types, description} = req.body;
+        const { name, price, category, types, description } = req.body;
         db.Productos.create({
             name,
             description,
             price,
-            image:req.files[0].filename,
+            image: req.files[0].filename,
             category_id: category,
-            client_id : req.session.user.client_id
+            client_id: req.session.user.client_id
         })
-        .then((product)=>{
-            const typesArray= [...(types.length ? types : [types])] 
-            /* preguntamos si lo que llega por parametro es un array o no, si es un array, lo guardamos
-             en la constante typesArray, si no es un array, lo convertimos a array y lo guardamos*/
+            .then((product) => {
+                const typesArray = [...(types.length ? types : [types])]
+                /* preguntamos si lo que llega por parametro es un array o no, si es un array, lo guardamos
+                 en la constante typesArray, si no es un array, lo convertimos a array y lo guardamos*/
 
 
-            /* console.log(typesArray); */
-            if(typesArray){
+                /* console.log(typesArray); */
+                if (typesArray) {
 
-                /* Recorremos el array y vamos cargando esos datos en la tabla pivot */
-                const createTypes = typesArray.map((type)=>{
-                    return db.TipoProductos.create({
-                        product_id: product.id,
-                        type_id: type
+                    /* Recorremos el array y vamos cargando esos datos en la tabla pivot */
+                    const createTypes = typesArray.map((type) => {
+                        return db.TipoProductos.create({
+                            product_id: product.id,
+                            type_id: type
+                        })
                     })
-                })
-                /* console.log(createTypes); */
-                Promise.all(createTypes)
-                .then(()=>{
+                    /* console.log(createTypes); */
+                    Promise.all(createTypes)
+                        .then(() => {
+                            res.redirect('/admin/list')
+                        })
+                        .catch(error => { res.send(error) })
+                } else {
                     res.redirect('/admin/list')
-                })
-                .catch(error=>{res.send(error)})
-            }else{
-                res.redirect('/admin/list')
-            }
-            
-        })
-        .catch((error)=>{res.send(error)})        
+                }
+
+            })
+            .catch((error) => { res.send(error) })
     },
     editProduct: (req, res) => {
-        db.Productos.findByPk(req.params.id)
+        let product = db.Productos.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        let categories = db.Categorias.findAll();
+        let types = db.Tipos.findAll();
+
+        Promise.all([product, categories, types])
+            .then(([product, categories, types]) => {
+                res.render("admin/productEdit", {
+                    product,
+                    categories,
+                    types
+                })
+            })
+            .catch((error) => {res.send(error)})
+        /* db.Productos.findByPk(req.params.id)
             .then((product) => {
                 res.render("admin/productEdit", {product})
             })
-            .catch((error) => {res.send(error)})
+            .catch((error) => {res.send(error)}) */
     },
     updateProduct: (req, res, next) => {
 
-        /* const { name, price, client, image, category, vegan, vegetarian, celiac } = req.body;
+        const { name, price, category, description } = req.body;
+        const { id } = req.params;
 
-        products.forEach(product => {
-            if (product.id === +req.params.id) {
-                if (fs.existsSync(path.join('public', 'images', 'products', product.image))) {
-                    fs.unlinkSync(path.join('public', 'images', 'products', product.image));
-                }
-                product.id = +req.params.id;
-                product.name = name;
-                product.price = price;
-                product.client = client;
-                product.image = req.files[0].filename;
-                product.category = category;
-                product.vegan = vegan;
-                product.vegetarian = vegetarian;
-                product.celiac = celiac
-
+        db.Productos.update({
+            name,
+            description,
+            price,
+            image: req.files[0].filename,
+            category_id: category,
+            client_id: req.session.user.client_id
+        }, {
+            where: {
+                id: id
             }
-        });
-        setProduct(products)
-        res.redirect('/admin/list'); */
+        })
+            .then(() => {
+                res.redirect('/admin/list');
+            })
+            .catch(error => res.send(error))
     },
     productDelete: (req, res) => {
         db.Productos.destroy({
@@ -97,7 +112,7 @@ const adminController = {
                 return res.redirect('/admin/list');
             })
             .catch(error => res.send(error))
-    } 
+    }
 }
 
 module.exports = adminController
