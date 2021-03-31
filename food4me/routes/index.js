676655421edff80c841022eece80b176e8a9f235
belenbernal <beelu.bernal@gmail.com>
2021-03-30 21:21:54 -0300

    arrelo en rutas

--- food4me/routes/index.js
+++ food4me/routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 const {menu,contact,home, contactForm} = require('../controllers/indexController');
 
-const contactValidator = require('../validations/contactValidator');
+/* const contactValidator = require('../validations/contactValidator'); */
 
 /* GET home page. */
 router.get('/', home);
 
 router.get('/menu', menu);
 
-router.get('/contact', contactValidator, contact);
-router.post('/contact', contactValidator, contactForm);
+router.get('/contact', /* contactValidator, */ contact);
+router.post('/contact', /* contactValidator, */ contactForm);
 
 module.exports = router;
